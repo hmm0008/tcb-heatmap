63e64aa61edbec9b411ebddbbc890204cdb92e0b
hassan muhammad <hmm0008@uah.edu>
2020-03-05 19:06:50 -0600

    add data and code for tcb visualization

---
+++ config.py
@@ -0,0 +1,10 @@
+# Static Global Variables
+
+WINTER = ['dec19.json', 'jan20.json', 'feb20.json']
+SPRING = ['march19.json', 'april19.json', 'may19.json']
+SUMMER = ['june19.json', 'july19.json', 'august19.json']
+FALL = ['sept19.json', 'oct19.json', 'nov19.json']
+HURRICANE = ['june19.json', 'july19.json', 'august19.json', 'sept19.json', 'oct19.json', 'nov19.json']
+ALL_MONTHS = ['dec19.json', 'jan20.json', 'feb20.json', 'march19.json', 'april19.json', 'may19.json', 'june19.json', 'july19.json', 'august19.json', 'sept19.json', 'oct19.json', 'nov19.json']
+
+SIZE_OF_GRID = 1
---
+++ seasonal.py
@@ -0,0 +1,68 @@
+import json
+import numpy as np
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+
+from config import *
+
+
+LATLIST = []
+LNGLIST = []
+
+# atlantic boundaries:
+# lon: -81, 39
+
+# east pacific boundaries:
+# lon: -152, -70
+
+# west pacific boundaries:
+# lon: 140, -152
+
+def get_center(c):
+    latSum = 0
+    lngSum = 0
+
+    for coor in c:
+        latSum += coor[0]
+        lngSum += coor[1]
+
+    lat = latSum/len(c)
+    lng = lngSum/len(c)
+
+    return lat, lng
+
+def parse_json(json_list):
+    for season in json_list:
+        with open(f'./data/{season}') as season:
+            data = json.load(season)
+            for data_point in data:
+                entry = data_point['bbox']['coordinates'][0]
+                lng, lat = get_center(entry)
+
+                # if lng >= -81 and lng <= 39:
+                LATLIST.append(np.floor(lat))
+                LNGLIST.append(np.floor(lng))
+
+def draw_map():
+    cmblist = zip(LATLIST, LNGLIST)
+    longitude_size = np.arange(-180, 180, SIZE_OF_GRID)
+    latitude_size = np.arange(-90, 90, SIZE_OF_GRID)
+
+    longitude_mesh, latitude_mesh = np.meshgrid(longitude_size, latitude_size)
+
+    dens = np.zeros((int(180/SIZE_OF_GRID), int(360/SIZE_OF_GRID)))
+
+    for lat, lon in cmblist:
+        dens[int((lat+90)/SIZE_OF_GRID), int((lon+180)/SIZE_OF_GRID)] += 1
+
+    ax = plt.axes(projection = ccrs.PlateCarree())
+    plt.contourf(longitude_mesh, latitude_mesh, dens, 10, transform=ccrs.PlateCarree())
+
+    ax.coastlines()
+    plt.show()
+
+if __name__ == "__main__":
+    
+    parse_json(ALL_MONTHS)
+    draw_map()
+    
